9b61c753495a2aca622d1b4be219116f573a9ed1
José Márquez Doblas <pepemarquezof@gmail.com>
2019-03-18 20:02:21 +0100

    api

--- Addon/transparentia.js
+++ Addon/transparentia.js
@@ -2,23 +2,12 @@ var space = document.createElement("br");
 
 var name = document.getElementsByClassName("ProfileHeaderCard-nameLink u-textInheritColor js-nav");
 var RealName = name[0];
-var salary;
 
-$.ajax({
-    url: '/GiveMeMoney',
-    data:{ param: RealName.textContent},
-    type: 'POST',
-    success: function(response){
-        console.log(response);
-        salary = response.salary;
-    },
-    error: function(error){
-        console.log(error);
-    }
-});
+http://localhost:5000/get_name_parsed
+
 
 var sueldo = document.createElement("a");
-sueldo.textContent = salary;
+sueldo.textContent = parse_name;
 sueldo.href = "https://www.newtral.com/";
 
 var name = document.getElementsByClassName("ProfileHeaderCard-name");
